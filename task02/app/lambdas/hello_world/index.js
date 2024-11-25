e0ebdfea3bd4f436ebe6c5dcae9a71e1a7df4f0d
Margarita Mkrtchyan <margarita_mkrtchyan@epam.com>
2024-11-25 17:49:41 +0400

    edit get request

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -2,15 +2,21 @@ exports.handler = async (event) => {
     if (event.rawPath === '/hello' && event.requestContext.http.method === 'GET') {
         const response = {
             statusCode: 200,
-            message: 'Hello from Lambda', // Top-level field
+            body: JSON.stringify({
+                statusCode: 200,
+                message: 'Hello from Lambda',
+            }),
         };
+        
         return response;
     }
 
     const response = {
         statusCode: 400,
-        message: `Bad request syntax or unsupported method. Request path: ${event.rawPath}. HTTP method: ${event.requestContext.http.method}`, // Top-level field
+        body: JSON.stringify({
+            statusCode: 400,
+            message: `Bad request syntax or unsupported method. Request path: ${event.rawPath}. HTTP method: ${event.requestContext.http.method}`,
+        }),
     };
     return response;
 };
-
