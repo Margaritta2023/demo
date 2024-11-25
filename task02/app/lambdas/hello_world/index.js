7eb6190490afeea22b2fbe19fd9b34a5da952dd1
Margarita Mkrtchyan <margarita_mkrtchyan@epam.com>
2024-11-25 17:12:25 +0400

    edit get request

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -3,13 +3,13 @@ exports.handler = async (event) => {
     if (event.rawPath === '/hello' && event.requestContext.http.method === 'GET') {
         const response = {
             statusCode: 200,
-            
-            message: 'Hello from Lambda',
-        
+            body: JSON.stringify({
+                message: 'Hello from Lambda', 
+            }),
         };
         return response;
     }
- 
+
     const response = {
         statusCode: 400,
         body: JSON.stringify({
